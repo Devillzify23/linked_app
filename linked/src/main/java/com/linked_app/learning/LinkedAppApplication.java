079793f49ee83719a3379a2cd1e694aad3721da4
Andrés Muñoz Pérez <devil1995@hotmail.es>
2024-06-09 15:00:33 +0200

    spring-base
    project

---
+++ linked/src/main/java/com/linked_app/learning/LinkedAppApplication.java
@@ -0,0 +1,13 @@
+package com.linked_app.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedAppApplication.class, args);
+	}
+
+}
---
+++ linked/src/test/java/com/linked_app/learning/LinkedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linked_app.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
